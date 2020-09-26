c405d48d38c5eb5c669dbe4125261d00b6d71643
Semen Zazulin <zazulinsemen@gmail.com>
2020-09-26 22:40:02 +0300

    AlfaVersion

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,17 +1,12 @@
 public class CreditPaymentService {
-    public double calculate  (double credit, double term)
-    {
-        double interestRate = 9.99; // процентная ставка
-        double p1 = 1 + interestRate; // эта переменная нужна для формулы K = (interestRate * p1) / (p1 - 1)
-        //формулу взял с сайта (https://www.banki.ru/wikibank/annuitetnyie_plateji_po_kreditu/)
-        int p2 = 12; // количество месяцев за которые выплачивается кредит, это степень в которую возвожу p1
-        public static double pow (double p1, double p2); // возведение в степень для расчёта по формуле
-        {
-        return StrictMath.pow(p1, p2);
-        }
-        double K = (interestRate * p1) / (p1 - 1); //  коэффициент аннуитета
-        double S = credit;  // сумма кредита
-        double mounthlyPayment = K * S;// ежемесячный аннуитетный платёж = коэффициент аннуитета * сумма кредита
-        return mounthlyPayment;
+    public double calculate  (int credit, int term, double interestRate)
+    { // пользуюсь этой формулой расчёта аннуитетного платежа https://www.banki.ru/wikibank/raschet_annuitetnogo_plateja/
+        double mounthleInterestRate = interestRate / term;
+        double coefficient1 = 1 + mounthleInterestRate;
+        double coefficient2 = Math.pow(coefficient1, term);
+        double annuityСoefficient = (mounthleInterestRate * coefficient2) / ((coefficient2) - 1);
+        double doubleCreditPayment = annuityСoefficient * credit;
+        int creditPayment = (int) doubleCreditPayment;
+        return creditPayment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService(); // создание объекта из класса
-        double credit = 1_000_000; // 1 000 000 рублей кредит
-        double term = 12; // 12 месяцев срок погашения
-        double mounthlyPayment = service.calculate(credit, term);
-        System.out.println(mounthlyPayment);
+        int credit = 1_000_000; // 1 000 000 рублей кредит
+        int term = 12; // 12 месяцев срок погашения
+        double interestRate = 9.99; // процентная ставка
+        double mounthlyPayment = service.calculate(credit, term, interestRate);
+        System.out.printf("Месячный платёж в рублях %.2f", mounthlyPayment);
     }
 }
