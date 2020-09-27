1a4ac0f76f81b8a50e3c85989403275473bae7ef
Semen Zazulin <zazulinsemen@gmail.com>
2020-09-27 11:44:33 +0300

    ServiceCalculate

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,12 +1,9 @@
 public class CreditPaymentService {
-    public double calculate  (int credit, int term, double interestRate)
-    { // пользуюсь этой формулой расчёта аннуитетного платежа https://www.banki.ru/wikibank/raschet_annuitetnogo_plateja/
-        double mounthleInterestRate = interestRate / term;
-        double coefficient1 = 1 + mounthleInterestRate;
-        double coefficient2 = Math.pow(coefficient1, term);
-        double annuityСoefficient = (mounthleInterestRate * coefficient2) / ((coefficient2) - 1);
-        double doubleCreditPayment = annuityСoefficient * credit;
-        int creditPayment = (int) doubleCreditPayment;
-        return creditPayment;
+    public int calculate(int S, int n, double interestRate) { // пользуюсь этой формулой расчёта аннуитетного платежа https://www.banki.ru/wikibank/raschet_annuitetnogo_plateja/
+        double i = interestRate / 100 / 12;
+        double coef1 = Math.pow(1 + i, n);
+        double doubleP = S * (i + (i / (coef1 - 1)));
+        int P = (int) doubleP;
+        return P;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService(); // создание объекта из класса
-        int credit = 1_000_000; // 1 000 000 рублей кредит
-        int term = 12; // 12 месяцев срок погашения
+        int S = 1_000_000; // 1 000 000 рублей кредит
+        int n = 12; // 12 месяцев срок погашения
         double interestRate = 9.99; // процентная ставка
-        double mounthlyPayment = service.calculate(credit, term, interestRate);
-        System.out.printf("Месячный платёж в рублях %.2f", mounthlyPayment);
+        System.out.println(" Месячный платёж в рублях " + service.calculate(S, n, interestRate));
+        n = 24;
+        System.out.println(" Месячный платёж в рублях " + service.calculate(S, n, interestRate));
+        n = 36;
+        System.out.println(" Месячный платёж в рублях " + service.calculate(S, n, interestRate));
     }
 }
